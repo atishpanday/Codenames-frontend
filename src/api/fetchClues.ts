237ff26d730631284cd0428d0e767a74c1ab5edc
Atish Panday <atishpanday@Atishs-MacBook-Air.local>
2025-04-18 15:06:52 -0700

    centralized env var

--- src/api/fetchClues.ts
+++ src/api/fetchClues.ts
@@ -1,26 +1,31 @@
 import axios from "axios";
 import { CardType, RoundType } from "../types/dataTypes.ts";
 import { SetStateAction } from "react";
+import { backendUrl } from "../env_var.ts";
 
 const fetchClues = async (
-    cards: CardType[],
-    round: RoundType,
-    setRound: (round: SetStateAction<RoundType>) => void) => {
-    setRound(prev => ({ ...prev, isFetchingClues: true }));
-    try {
-        const response = await axios.post("http://127.0.0.1:5000/get-clue?turn=" + round.turn, cards);
-        const data = response.data;
-        console.log(data);
-        setRound({
-            turn: round.turn,
-            clue: data.clue,
-            numIntendedWords: data.num_intended_words,
-            numCardsLeft: data.num_intended_words,
-            isFetchingClues: false,
-        });
-    } catch (error) {
-        console.log("Error fetching clue");
-    }
-}
+  cards: CardType[],
+  round: RoundType,
+  setRound: (round: SetStateAction<RoundType>) => void
+) => {
+  setRound((prev) => ({ ...prev, isFetchingClues: true }));
+  try {
+    const response = await axios.post(
+      backendUrl + "/get-clue?turn=" + round.turn,
+      cards
+    );
+    const data = response.data;
+    console.log(data);
+    setRound({
+      turn: round.turn,
+      clue: data.clue,
+      numIntendedWords: data.num_intended_words,
+      numCardsLeft: data.num_intended_words,
+      isFetchingClues: false,
+    });
+  } catch (error) {
+    console.log("Error fetching clue");
+  }
+};
 
 export default fetchClues;
--- src/api/fetchWords.ts
+++ src/api/fetchWords.ts
@@ -2,21 +2,31 @@ import axios from "axios";
 import { CardType, RoundType } from "../types/dataTypes.ts";
 import { shuffleArray } from "../utils/shuffleArray.ts";
 import { SetStateAction } from "react";
+import { backendUrl } from "../env_var.ts";
 
-const fetchWords = async (setCards: (data: SetStateAction<CardType[]>) => void, setRound: (round: SetStateAction<RoundType>) => void) => {
-    const turn = Math.random() > 0.5 ? "red" : "blue";
-    try {
-        const response = await axios.get("http://127.0.0.1:5000/", {
-            params: {
-                first: turn,
-            },
-        });
-        const data = response.data;
-        setCards(shuffleArray(data));
-        setRound({ turn: turn, clue: "", numCardsLeft: 0, numIntendedWords: 0, isFetchingClues: false });
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
+const fetchWords = async (
+  setCards: (data: SetStateAction<CardType[]>) => void,
+  setRound: (round: SetStateAction<RoundType>) => void
+) => {
+  const turn = Math.random() > 0.5 ? "red" : "blue";
+  try {
+    const response = await axios.get(backendUrl + "/", {
+      params: {
+        first: turn,
+      },
+    });
+    const data = response.data;
+    setCards(shuffleArray(data));
+    setRound({
+      turn: turn,
+      clue: "",
+      numCardsLeft: 0,
+      numIntendedWords: 0,
+      isFetchingClues: false,
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
 };
 
 export default fetchWords;
